a377d19824bda1d6cc436d42ec9934fe642a5e17
IsmaelLebzo <20017635@student.ltuc.com>
2021-09-29 17:15:02 +0300

    finished lab12

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,25 +2,26 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
-import Card from 'react-bootstrap/Card';
 import axios from 'axios';
 import { withAuth0 } from '@auth0/auth0-react';
-
-
+import Book from './components/Book';
+import CardInfo from './components/CardInfo';
+import Form from './components/Form';
 
 class MyFavoriteBooks extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       BookData: [],
-      searchQuery: ''
+      searchQuery: '',
+      showBookInfo: false
     }
   }
-compMount = async () => {
+componentDidMount = async () => {
   let email1 = this.props.auth0.user.email;
   let BookUrl = `${process.env.REACT_APP_SERVER_LINK}/books?email=${email1}`;
   let bokData = await axios.get(BookUrl);
-    await this.setState({
+    this.setState({
     BookData: bokData.data
   })
   console.log(this.state.BookData);
@@ -28,6 +29,27 @@ compMount = async () => {
   console.log(bokData);
 }
 
+addBook = async(e) =>{
+  e.preventDefault();
+
+  let bookInfoData = {
+    title: e.target.title.value,
+    description: e.target.description.value,
+    status: e.target.description.value,
+    email: this.props.auth0.user.email
+  }
+  let newBook = await axios.post(`${process.env.REACT_APP_SERVER_LINK}/addBook`, bookInfoData);
+  this.setState({
+    BookData: newBook.data
+  })
+}
+
+deleteBook = async(id) =>{
+  let removeBook = await axios.delete(`${process.env.REACT_APP_SERVER_LINK}/deleteBooks?bookID=${id}&email=${this.props.auth0.user.email}`)
+  await this.setState({
+    BookData: removeBook.data
+  })
+}
 
   render() {
     
@@ -39,26 +61,18 @@ compMount = async () => {
         <p>
           This is a collection of my favorite books
         </p>
-      </Jumbotron>
-      {this.state.BookData.map((ele, index) => {
-        return(
-          <Card key={index} style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title>{ele.title}
-          </Card.Title>
-          <Card.Text>
-          {ele.description}
-          </Card.Text>
-          <Card.Text>
-          {ele.status}
-          </Card.Text>
-          <Card.Text>
-          {ele.email}
-          </Card.Text>
-          </Card.Body>
-      </Card>
-        )
+        <Form bookAddFun = {this.addBook}/>
+        {this.state.BookData.map((ele, idx) => {
+        return <CardInfo key={idx} element={ele} DeleteBook={this.deleteBook} />
       })}
+        {this.state.showBookInfo && 
+      this.state.book.map((item, idx) => {
+        return(<Book key={idx} book={item} DeleteBook={this.deleteBook} />)
+      })
+      }
+      
+      </Jumbotron>
+     
       </>
     )
   }
---
+++ src/components/CardInfo.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+class CardInfo extends React.Component{
+    render(){
+      console.log(this.props);
+        return(
+          <>
+        <Card key={this.props.element.index} style={{ width: '18rem' }}>
+          <Card.Body>
+             <Card.Title>
+                 {this.props.element.title}
+             </Card.Title>
+            <Card.Text>
+                 {this.props.element.description}
+            </Card.Text>
+            <Card.Text>
+                 {this.props.element.status}
+            </Card.Text>
+            <Button onClick={()=>{this.props.DeleteBook(this.props.element._id)}}> X </Button>
+          </Card.Body>
+        </Card>
+          </>  
+        )
+    }
+}
+export default CardInfo;
